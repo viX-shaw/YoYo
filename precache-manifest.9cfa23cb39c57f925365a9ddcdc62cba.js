b44e5d19ed3905b09764a1afa3f8840a74874aa5
viX-shaw <songokurocks551@gmail.com>
2020-02-03 10:50:59 +0530

    Updates

---
+++ precache-manifest.9cfa23cb39c57f925365a9ddcdc62cba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04121c280acbd95a78d5d0b5d80d9cd7",
+    "url": "/YoYo/index.html"
+  },
+  {
+    "revision": "6ee91e1e75abb0cc75f2",
+    "url": "/YoYo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fb6459c866fadb4aadda",
+    "url": "/YoYo/static/js/2.162b509e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/YoYo/static/js/2.162b509e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee91e1e75abb0cc75f2",
+    "url": "/YoYo/static/js/main.5085f70e.chunk.js"
+  },
+  {
+    "revision": "bfc791891f4dea782bf3",
+    "url": "/YoYo/static/js/runtime-main.8b7d7f95.js"
+  },
+  {
+    "revision": "cc7a458d04f778d9d8abcf0361cce335",
+    "url": "/YoYo/static/media/chad-montano-MqT0asuoIcU-unsplash.cc7a458d.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/YoYo/static/media/logo.5d5d9eef.svg"
+  }
+]);
